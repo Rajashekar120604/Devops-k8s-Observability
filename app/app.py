e66d5250429c679096a029a1e434d10092a8f0af
Rajashekar120604 <126018036@sastra.ac.in>
2025-12-23 15:25:44 +0530

    App version 3.0 updated

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, render_template
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import logging
+import json
+
+# 🔥 IMPORTANT: raw JSON logs (no prefixes)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    force=True
+)
+
+def log_event(level, event, **kwargs):
+    logging.log(level, json.dumps({
+        "event": event,
+        **kwargs
+    }))
 
 app = Flask(__name__)
 
 login_attempts = Counter(
-    "login_attempts",
+    "login_attempts_total",
     "Total number of login attempts",
     ["result"]
 )
@@ -17,18 +32,13 @@ def login():
 
         if username == "admin" and password == "admin":
             login_attempts.labels(result="success").inc()
-            return render_template(
-                "login.html",
-                error=" Login successful"
-            )
+            log_event(logging.INFO, "login_success", username=username)
+            return render_template("login.html", error="Login successful")
         else:
             login_attempts.labels(result="failure").inc()
-            return render_template(
-                "login.html",
-                error=" Invalid credentials"
-            )
+            log_event(logging.WARNING, "login_failure", username=username)
+            return render_template("login.html", error="Invalid credentials")
 
-    # GET request → just show login page
     return render_template("login.html")
 
 @app.route("/metrics")
